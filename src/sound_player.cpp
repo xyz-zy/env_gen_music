2232f57ed7960f9833552abfc16583eb75dfe625
Smitha <sm.nn88@gmail.com>
2017-05-09 13:34:54 -0500

    got sound play to work, changed path names

--- src/sound_player.cpp
+++ src/sound_player.cpp
@@ -7,36 +7,36 @@
 #include <vector>
 #include <iostream>
 
-char category_buffer[20];
+char category_buffer[40];
 ros::Subscriber colormood_sub;
 
 void colormood_callback(const env_gen_music::colormood::ConstPtr& msg) {
 	int numSongs = 4;
-	int random = (rand() * 3) + 1;
+	int random = (rand() % 4) + 1;
 
 	if (msg->happiness == 1) { 
 		if (msg->tempo == 1) { 
 			//happy_fast
-			sprintf(category_buffer, "happy_fast%d.wav", random);	
+			sprintf(category_buffer, "music/happy_fast%d.wav", random);	
 		}
 		else {
 			//happy_slow
-			sprintf(category_buffer, "happy_slow%d.wav", random);	
+			sprintf(category_buffer, "music/happy_slow%d.wav", random);	
 		}
 	}
 	else {
 		if (msg->tempo == 1) { 
 			//sad_fast	
-			sprintf(category_buffer, "sad_fast%d.wav", random);	
+			sprintf(category_buffer, "music/sad_fast%d.wav", random);	
 		}
 		else {
 			// sad_slow
-			sprintf(category_buffer, "sad_slow%d.wav", random);	
+			sprintf(category_buffer, "music/sad_slow%d.wav", random);	
 		}
 	}
 
 	//debug statements
-	ROS_INFO("random: %d, string: %s\n", random, category_buffer);
+//	ROS_INFO("random: %d, string: %s\n", random, category_buffer);
 
 	//if (person) {
 		// person
@@ -45,16 +45,16 @@ void colormood_callback(const env_gen_music::colormood::ConstPtr& msg) {
 
 int main(int argc, char **argv) {
 	
-	sound_play::SoundClient sc;
 	ros::init(argc, argv, "sound_player");
+	sound_play::SoundClient sc;
 	ros::NodeHandle n;
 	colormood_sub = n.subscribe("/colormood", 1, colormood_callback);
-	ros::Rate r(100);
+	ros::Rate r(10000);
 
 	while (ros::ok()) {
     		ros::spinOnce();
 		printf("%s\n", category_buffer);
-    		sc.playWaveFromPkg("sound_play", category_buffer);
+    		sc.playWaveFromPkg("env_gen_music", category_buffer);
     		r.sleep();
 		// TODO how do we wait for 20 seconds for song to play?
 	}
